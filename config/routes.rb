d755c1e5f42cc4ee680d48706a3ce576d562ba97
Frank Fenton <echelon.iiii@gmail.com>
2016-09-21 21:39:38 +1000

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'front/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'front/index'
+  
   root 'front#index'
 end
