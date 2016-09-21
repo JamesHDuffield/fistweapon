6a045a5ba6b214a86b1742509d44fc90b545301b
James Duffield <james.h.duffield@gmail.com>
2016-09-21 18:06:53 +1000

    Add battlenet config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,11 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Battlenet.configure do |config|
+    config.api_key = 'ng6bgwrqguymnyh6uufcmds5f4nr3hde'
+    config.region  = :us
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
