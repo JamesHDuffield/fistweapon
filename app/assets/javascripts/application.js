5654932f4e8628a50f002597ac0b843b4c4bc840
James Duffield <james.h.duffield@gmail.com>
2016-09-22 15:52:38 +1000

    Adding jquery filtering like a boss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,29 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+var greens_hidden = false;
+function toggle_greens() {
+  greens_hidden = !greens_hidden;
+  if(greens_hidden){
+    $(".q2").parent().parent().hide();
+    $("#tgreen").css("text-decoration", "line-through");
+  }
+  else {
+    $(".q2").parent().parent().show();
+    $("#tgreen").css("text-decoration", "none");
+  }
+}
+
+var blues_hidden = false;
+function toggle_blues() {
+  blues_hidden = !blues_hidden;
+  if(blues_hidden){
+    $(".q3").parent().parent().hide();
+    $("#tblue").css("text-decoration", "line-through");
+  }
+  else {
+    $(".q3").parent().parent().show();
+    $("#tblue").css("text-decoration", "none");
+  }
+}
