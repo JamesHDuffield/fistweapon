988be138102ba566a40eb57b4baa9ca6710ad5c5
James Duffield <james.h.duffield@gmail.com>
2016-09-23 13:12:22 +1000

    Make titles dynamic from db calls

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -12,4 +12,22 @@ module FrontHelper
   def render_log_link
     content_tag(:a, 'View Logs', href: "https://www.warcraftlogs.com/guilds/usercalendar/197620")
   end
+
+  def guild_name
+    Rails.application.config.guild_name
+  end
+
+  def realm
+    Rails.application.config.realm
+  end
+
+  def allegiance
+    g = Guild.find_by(name: Rails.application.config.guild_name)
+    if g != nil && g.side == 1
+      content_tag(:div, 'Horde Guild', class: "guild-side horde")
+    else
+      content_tag(:div, 'Aliiance Guild', class: "guild-side alliance")
+    end
+  end
+
 end
