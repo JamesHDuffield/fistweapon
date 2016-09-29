91d65b6749167b4e415f0fbd7a57480e35d0a0a1
James Duffield <james.h.duffield@gmail.com>
2016-09-29 14:28:51 +1000

    Remove all testing

--- test/controllers/front_controller_test.rb
+++ test/controllers/front_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class FrontControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get front_index_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
