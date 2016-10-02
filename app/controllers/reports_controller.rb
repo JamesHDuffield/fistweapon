a5002853a939d1d6d473290c72118a1717e6f90f
James Duffield <james.h.duffield@gmail.com>
2016-10-03 10:52:02 +1100

    Revert reports to be simpler, ignore local.yml file

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,20 +1,12 @@
 class ReportsController < ApplicationController
-  def members_list
-	   if params[:term]
-       like= "%".concat(params[:term].concat("%"))
-       users = Member.where("name like ?", like)
-     else
-       users = Member.all
-     end
-     list = users.map {|u| Hash[ id: u.id, label: u.name, name: u.name]}
-     render json: list
-  end
-
   def new
+    @report = Report.new
     @members = Member.order('name').all
-
   end
 
   def create
+  end
+
+  def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  post '/reports', to: 'reports#create'
-  get '/reports/new', to: 'reports#new'
+  resources :reports
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'reports/members_list', to: 'reports#members_list'
   get '/reset_cache', to: 'front#reset_cache'
   root 'front#index'
 end
