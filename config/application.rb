5c16b937bb44b7d59919ef6a810521ac8cc54471
James Duffield <james.h.duffield@gmail.com>
2016-10-03 10:45:22 +1100

    Better error handling on environment variables and local config option added

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
+def get_env(key)
+  if ENV[key] == nil
+    throw "Enviroment key #{key} not set"
+  end
+  return ENV[key]
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,18 +18,24 @@ module Fistweapon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     Battlenet.configure do |config|
-      config.api_key = ENV['BNET_API_KEY']
-      config.region = ENV['BNET_REGION'].to_sym
+      config.api_key = get_env('BNET_API_KEY')
+      config.region = get_env('BNET_REGION').to_sym
     end
 
     Rails.application.configure do
       config.after_initialize do
-        config.realm = ENV['WOW_REALM']
-        config.guild_name = ENV['WOW_GUILD']
-        config.character_name = ENV['WOW_CHARACTER_NAME'] #Used for progression lookup (use guild leader generally)
-        config.raids = ENV['WOW_RAIDS'].split(",")
+        config.realm = get_env('WOW_REALM')
+        config.guild_name = get_env('WOW_GUILD')
+        config.character_name = get_env('WOW_CHARACTER_NAME') #Used for progression lookup (use guild leader generally)
+        config.raids = get_env('WOW_RAIDS').split(",")
         config.amr_url_base = 'http://www.askmrrobot.com/wow/gear/us'
         config.media_url_base = 'http://media.blizzard.com/wow/icons/56'
         config.cache_members = lambda { 1.days.ago }
@@ -30,8 +43,8 @@ module Fistweapon
         config.cache_discord = lambda { 1.minutes.ago }
         config.cache_progression = lambda { 1.days.ago }
 
-        config.discord_channel_id = ENV['DISCORD_CHANNEL_ID']
-        config.discord_key = ENV['DISCORD_KEY']
+        config.discord_channel_id = get_env('DISCORD_CHANNEL_ID')
+        config.discord_key = get_env('DISCORD_KEY')
       end
     end
 
