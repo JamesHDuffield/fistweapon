4e78191aebee3d2590d18f0197ea8353ad07ea04
James Duffield <james.h.duffield@gmail.com>
2016-10-04 10:38:54 +1100

    Darken the guild colour a little

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,19 @@ module ApplicationHelper
     Rails.application.config.guild_name
   end
 
+  def darken_color(hex_color, amount=0.6)
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = (rgb[0].to_i * amount).round
+    rgb[1] = (rgb[1].to_i * amount).round
+    rgb[2] = (rgb[2].to_i * amount).round
+    "#%02x%02x%02x" % rgb
+  end
+
   def guild_colour
     g = Guild.find_by(name: Rails.application.config.guild_name)
     if g == nil
       return '#000'
     end
-    return "##{g.background_colour.last(6)}"
+    return darken_color(g.background_colour.last(6))
   end
 end
