d5b205f6b24622601dbf8dc56acd243f01f3f382
James Duffield <james.h.duffield@gmail.com>
2016-10-03 11:48:23 +1100

    Change primary colour depending on guild colours

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def guild_name
     Rails.application.config.guild_name
   end
+
+  def guild_colour
+    g = Guild.find_by(name: Rails.application.config.guild_name)
+    if g == nil
+      return '#000'
+    end
+    return "##{g.background_colour.last(6)}"
+  end
 end
