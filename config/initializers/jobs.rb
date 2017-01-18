8a08a94cf4d32325be657d0ae87d805687d55f4f
James Duffield <james.duffield@myriad-it.com>
2017-01-18 16:26:11 +1100

    Only start delayed worker tasks is running delayed worker

--- config/initializers/jobs.rb
+++ config/initializers/jobs.rb
@@ -1,6 +1,8 @@
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
-Delayed::Job.destroy_all
-EventUpdateJob.perform_later
-MemberUpdateJob.perform_later
-DiscordUpdateJob.perform_later
-ProgressionUpdateJob.perform_later
+if Rake.application.top_level_tasks.include?("jobs:work")
+    Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
+    Delayed::Job.destroy_all
+    EventUpdateJob.perform_later
+    MemberUpdateJob.perform_later
+    DiscordUpdateJob.perform_later
+    ProgressionUpdateJob.perform_later
+end
